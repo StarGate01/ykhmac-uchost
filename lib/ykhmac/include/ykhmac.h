81feee6064f23cb7538c0ec1c725b85c5e931e1d
Christoph Honal <christoph.honal@web.de>
2021-12-17 13:22:13 +0100

    Initialize library

---
+++ lib/ykhmac/include/ykhmac.h
@@ -0,0 +1,12 @@
+/**
+ * @file ykhmac.h
+ * @author Christoph Honal
+ * @brief Defines the Yubikey HMAC-SHA1 logic
+ * @version 0.1
+ * @date 2021-12-17
+ */
+
+#ifndef YKHMAC_H
+#define YKHMAC_H
+
+#endif
---
+++ lib/ykhmac/src/ykhmac.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file ykhmac.cpp
+ * @author Christoph Honal
+ * @brief Implements the definitions from ykhmac.h
+ * @version 0.1
+ * @date 2021-12-17
+ */
+
+#include "ykhmac.h"
+
+#include <sha/sha1.h>
